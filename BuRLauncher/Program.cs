8033db87221aed45d2080a229ba5295cb2734552
Ľubo <lubo.bubco@gmail.com>
2017-09-12 10:09:29 +0200

    chrome bug fixed!

--- BuRLauncher/Program.cs
+++ BuRLauncher/Program.cs
@@ -9,7 +9,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //OPCServer objOPCServer;
+
         // Create UDP client
         int receiverPort = 11000;
         int batPort = 11001;
@@ -27,14 +27,14 @@ class Program
         }
         catch (Exception e)
         {
-            Console.WriteLine("Error:" + e.ToString() + "\n");    
+            Console.WriteLine("Error:" + e.ToString() + "\n");
         }
         Console.ReadKey();
         //try
-       // {
+        // {
         //    OPCServer m_OPCServer = new OPCServer();
-       // }
-        
+        // }
+
     }
 
     private static void DataReceived(IAsyncResult ar)
@@ -44,40 +44,53 @@ class Program
         Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
         int i = 0, j = 0;
         string recText;
+        bool isChromeOpen = false;
 
-        // Convert data to ASCII
-        recText = System.Text.Encoding.ASCII.GetString(receivedBytes);
+        recText = System.Text.Encoding.ASCII.GetString(receivedBytes);      //Convert Byte Array to ASCII String
         if (recText[0] != '\0')
-        {
-            while ((i < 500) && (receivedBytes[i] != ';'))
+        {                                            //Try empty message
+            while ((i < (receivedBytes.Length)) && (receivedBytes[i] != ';'))                  //wait for ';'
                 i++;
-            char[] path = new char[i];
-            recText.CopyTo(0, path, 0, i);
-            string FileName = new string(path);
-            if (recText[i] == ';')
+            char[] path = new char[i];                                      //Alloc storage of char array for path
+            recText.CopyTo(0, path, 0, i);                                  //Copy char array
+            string FileName = new string(path);                             //Convert char array to string
+            if (recText[i] == ';')                                          //try for empty arguments text
                 i++;
-            j = i;
-            while ((i < 500) && (receivedBytes[i] != '\0'))
+            j = i;                                                          //Save start position index of arguments string
+            while ((i < (receivedBytes.Length)) && (receivedBytes[i] != '\0'))                 //wait for end of string
                 i++;
-            char[] arguments = new char[i - j];
+            char[] arguments = new char[i - j];                             //Alloc storage of char array for arguments
             recText.CopyTo(j, arguments, 0, i - j);
             string Arguments = new string(arguments);
 
-            Process m_Proccess = new Process();
-            m_Proccess.StartInfo.FileName = FileName;
-            m_Proccess.StartInfo.Arguments = Arguments;
             try
             {
-                using (Process execute = Process.Start(m_Proccess.StartInfo))
+                if (FileName.CompareTo("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe") == 0)
                 {
-                    execute.WaitForExit();
+                    Process[] findChrome = Process.GetProcessesByName("chrome");
+                    if (findChrome.Length != 0)
+                        isChromeOpen = true;
+                }
+
+                if (!isChromeOpen)
+                {
+                    Process m_Proccess = new Process();
+                    m_Proccess.StartInfo.FileName = FileName;
+                    m_Proccess.StartInfo.Arguments = Arguments;
+                    Process.Start(m_Proccess.StartInfo);
+
                     Console.Write("app is opened in path: " + FileName + Environment.NewLine + "Arguments: " + Arguments + Environment.NewLine);
+                    Console.WriteLine("-------------------------------\n");
+                }
+                else
+                {
+                    Console.Write("Browser is already opened!" + Environment.NewLine);
                     Console.WriteLine("-------------------------------\n");
                 }
             }
             catch (Exception e)
             {
-                Console.Write("cannot open path: " + FileName + Environment.NewLine + "Arguments: " + Arguments + "Error: " +Environment.NewLine + e.ToString() + Environment.NewLine);
+                Console.Write("cannot open path: " + FileName + Environment.NewLine + "Arguments: " + Arguments + "Error: " + Environment.NewLine + e.ToString() + Environment.NewLine);
                 Console.WriteLine("-------------------------------\n");
             }
         }
